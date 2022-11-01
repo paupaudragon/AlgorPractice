de7a4752130674bedf583b50cd3a9e09307edc7b
Tingting Zhou <u1432080@umail.utah.edu>
2022-11-01 10:19:44 -0600

    leetcode26

---
+++ ElementarySorting/26DuplicateArray.java
@@ -0,0 +1,17 @@
+package ElementarySorting;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[j]!= nums[i]){
+                nums[j] = nums[i];
+                j++;
+        }
+        
+       }
+        return j;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Solution().removeDuplicates(new int[]{1,1,3,4,5,5}));}
+    
+}
