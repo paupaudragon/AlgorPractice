247c2bdec1a8670800f92b053df7297ffa6a3fca
Tingting Zhou <u1432080@umail.utah.edu>
2022-11-01 14:35:19 -0600

    Leetcode122 Best Time to Buy and Sell StockII Easy

---
+++ ElementarySorting/StockII.java
@@ -0,0 +1,15 @@
+package ElementarySorting;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        //this question is only asking to return the biggest profit not when to sell and buy. 
+        //So any time there is buy low sell high happens, we need to add that difference in price to our return value.
+        int profit = 0; 
+        for(int i =1; i < prices.length; i++) {
+            if(prices[i]>prices[i-1])
+                profit += prices[i]-prices[i-1];
+        }
+        return profit;
+    }
+}
