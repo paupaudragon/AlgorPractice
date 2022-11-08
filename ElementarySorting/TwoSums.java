7d575bb96ea26a33bc7245ded1e7262effb053dd
Tingting Zhou <u1432080@umail.utah.edu>
2022-11-08 14:46:52 -0700

    leetcode 1. Two Sum

---
+++ ElementarySorting/TwoSums.java
@@ -0,0 +1,25 @@
+package ElementarySorting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSums {
+   
+        public int[] twoSum(int[] nums, int target) {
+        
+            //Note that HashMap can only search value of a given key not the other way around.
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], i);
+            }
+            for (int i = 0; i < nums.length; i++) {
+                int temp = target - nums[i];
+                if (map.containsKey(temp) && map.get(temp) != i) {
+                    return new int[] { i, map.get(temp) };
+                }
+            }
+            // In case there is no solution, we'll just return null
+            return null;
+         
+        }
+    }
