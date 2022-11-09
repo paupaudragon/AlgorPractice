fcd5ace62913d87756d51429c74c0694f5e37c30
Tingting Zhou <u1432080@umail.utah.edu>
2022-11-09 15:16:37 -0700

    Array SingleNumber

---
+++ ElementarySorting/SingleNumber.java
@@ -0,0 +1,21 @@
+package ElementarySorting;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+    // didn't come up with a way that uses const extra space
+
+    // this is from online
+    //How XOR works: 
+    //1. a ^ 0 = 0
+    //2. a^b^b = b^a^b = a
+    public int singleNumber(int[] nums) {
+        int result =0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            result ^= nums[i];
+        }
+        return result;
+        
+    }
+}
